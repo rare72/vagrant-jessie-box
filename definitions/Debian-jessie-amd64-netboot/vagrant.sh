ad2d2803f1e1f2bdb74654f2c4959d50be222250
Phil Vinson <rare72@users.noreply.github.com>
2016-04-18 15:27:24 -0400

    Update vagrant.sh
    Inserted an space in the syntax for the following:
    
    Before: useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant
    After: useradd -G sudo -p $(perl -e 'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant
    
    This allowed me to set the password properly.

--- definitions/Debian-jessie-amd64-netboot/vagrant.sh
+++ definitions/Debian-jessie-amd64-netboot/vagrant.sh
@@ -3,7 +3,7 @@
 date > /etc/vagrant_box_build_time
 
 # Create the user vagrant with password vagrant
-useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant
+useradd -G sudo -p $(perl -e 'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant
 
 # Install vagrant keys
 mkdir -pm 700 /home/vagrant/.ssh
